27fe5bfa9d823a340059d67b8a017f24317786e3
tbswang <gt2017qd@163.com>
2021-02-04 09:30:58 +0800

    1742

---
+++ 1742.盒子中小球的最大数量.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=1742 lang=cpp
+ *
+ * [1742] 盒子中小球的最大数量
+ */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    int countBalls(int lowLimit, int highLimit) {
+        
+    }
+};
+// @lc code=end
+
+
+int main(){
+    Solution s;
+    cout << s.countBalls(1, 10) << endl;
+}
