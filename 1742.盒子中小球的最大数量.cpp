624f9aea3b3c4a318bca774f444dab446581d08b
tbswang <gt2017qd@163.com>
2021-02-04 13:13:11 +0800

    1742: unordered_map

--- 1742.盒子中小球的最大数量.cpp
+++ 1742.盒子中小球的最大数量.cpp
@@ -5,19 +5,44 @@
  */
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    int countBalls(int lowLimit, int highLimit) {
-        
+    int countBalls(int lowLimit, int highLimit)
+    {
+        unordered_map<int, int> box;
+        int i = lowLimit;
+        int out = 0;
+        while (i <= highLimit)
+        {
+            int idx = sum(i);
+            box[idx]++;
+            out = max(out, box[idx]);
+            i++;
+        }
+        return out;
+    }
+    int sum(int num)
+    {
+        int out = 0;
+        while (num > 0)
+        {
+            out += num % 10;
+            num = num / 10;
+        }
+        return out;
     }
 };
 // @lc code=end
 
-
-int main(){
+int main()
+{
     Solution s;
-    cout << s.countBalls(1, 10) << endl;
+    cout << s.countBalls(1, 10) << ' ';
+    cout << s.countBalls(5, 15) << ' ';
+    cout << s.countBalls(19, 28) << endl;
 }
