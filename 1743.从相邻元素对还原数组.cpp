009d18689c1fe330906d89b6cb322a6d37cc581e
tbswang <gt2017qd@163.com>
2021-02-06 20:30:24 +0800

    1743:超时o(n^2)

--- 1743.从相邻元素对还原数组.cpp
+++ 1743.从相邻元素对还原数组.cpp
@@ -4,7 +4,7 @@
  * [1743] 从相邻元素对还原数组
  */
 #include <iostream>
-#include <vector>
+#include <vector> 
 
 using namespace std;
 
@@ -13,19 +13,38 @@ class Solution {
 public:
     vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
         vector<int> res = adjacentPairs[0];
-        for (int i = 0; i < adjacentPairs.size(); i++) {
-            for (int j = i + 1; j < adjacentPairs.size(); j++) {
+        int start = 1;
+        while (start < adjacentPairs.size()) {
+            for (int i = start; i < adjacentPairs.size(); i++) {
                 // 头部 ,  左边
-                if (res[0] == adjacentPairs[j][0])
-                    res.insert(res.begin(), adjacentPairs[j][1]);
-                if (res[0] == adjacentPairs[j][1])
-                    res.insert(res.begin(), adjacentPairs[j][0]);
-                if (res.back() == adjacentPairs[j][0])
-                    res.push_back(adjacentPairs[j][1]);
-                if (res.back() == adjacentPairs[j][1])
-                    res.push_back(adjacentPairs[j][0]);
+                if (res[0] == adjacentPairs[i][0]) {
+                    res.insert(res.begin(), adjacentPairs[i][1]);
+                    adjacentPairs[i] = adjacentPairs[start];
+                    start++;
+                    break;
+                }
+                if (res[0] == adjacentPairs[i][1]) {
+                    res.insert(res.begin(), adjacentPairs[i][0]);
+                    adjacentPairs[i] = adjacentPairs[start];
+                    start++;
+                    break;
+                }
+                if (res.back() == adjacentPairs[i][0]) {
+
+                    res.push_back(adjacentPairs[i][1]);
+                    adjacentPairs[i] = adjacentPairs[start];
+                    start++;
+                    break;
+                }
+                if (res.back() == adjacentPairs[i][1]) {
+                    res.push_back(adjacentPairs[i][0]);
+                    adjacentPairs[i] = adjacentPairs[start];
+                    start++;
+                    break;
+                }
             }
         }
+
         return res;
     }
 };
@@ -33,7 +52,7 @@ public:
 
 int main() {
     Solution s;
-    vector<vector<int>> input = { {2,1}, {3,4}, {3,2} };
+    vector<vector<int>> input = { {100000,-100000} };
     vector <int> res = s.restoreArray(input);
     for (auto i : res) {
         cout << i << ' ';
