355039229ff84e7c6aad0fed3a7795ad20bf5792
tbswang <gt2017qd@163.com>
2021-02-08 22:44:14 +0800

    wip:114

--- 114.二叉树展开为链表.cpp
+++ 114.二叉树展开为链表.cpp
@@ -3,6 +3,12 @@
  *
  * [114] 二叉树展开为链表
  */
+
+#include<vector>
+#include<math.h>
+#include<queue>
+using namespace std;
+
 struct TreeNode {
     int val;
     TreeNode* left;
@@ -26,13 +32,38 @@ struct TreeNode {
 class Solution {
 public:
     void flatten(TreeNode* root) {
+        if (!root)
+            return;
+        if(root->left)
+            flatten(root->left);
+        if(root->right)
+            flatten(root->right);
+        TreeNode* rightest = getRightChild(root->left);
+        rightest->right = root->right;
         root->right = root->left;
         root->left = nullptr;
-        
     }
-    TreeNode * preOrder(){
-        
+    TreeNode* getRightChild(TreeNode* root) {
+        if (!root)
+            return root;
+        while (root->right) {
+            root = root->right;
+        }
+        return root;
+
     }
 };
 // @lc code=end
 
+int main(){
+    vector<int> list = {1,2,3,4,5,6,7};
+    auto root = new TreeNode(list[0]);
+    queue<TreeNode*> q;
+    q.push(root);
+    for(int i = 1; i< floor(log2(list.size() + 1)); i++){
+        auto now = q.front();
+        q.pop();
+        int levelSize = exp2(i);
+        for(int j = )
+    }
+}
