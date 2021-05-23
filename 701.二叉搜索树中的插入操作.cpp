ea394fc78ecdf6f49319a2a6e4dffbee6a0090ee
tbswang <gt2017qd@163.com>
2021-05-24 00:34:02 +0800

    701

---
+++ 701.二叉搜索树中的插入操作.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=701 lang=cpp
+ *
+ * [701] 二叉搜索树中的插入操作
+ */
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+  TreeNode *insertIntoBST(TreeNode *root, int val) {
+    if (root == nullptr)
+      return new TreeNode(val);
+    else if (root->val > val) {
+      root->left = insertIntoBST(root->left, val);
+    } else if (root->val < val) {
+      root->right = insertIntoBST(root->right, val);
+    }
+    // todo 写在里面和写在外面有啥区别
+    return root;
+  }
+};
+// @lc code=end
