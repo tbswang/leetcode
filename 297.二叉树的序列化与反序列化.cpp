8644251553e904b34aaa3d3ce126971dda623568
tbswang <gt2017qd@163.com>
2021-05-27 23:08:09 +0800

    NO:297

--- 297.二叉树的序列化与反序列化.cpp
+++ 297.二叉树的序列化与反序列化.cpp
@@ -3,6 +3,7 @@
  *
  * [297] 二叉树的序列化与反序列化
  */
+#include <iostream>
 #include <queue>
 #include <string>
 using namespace std;
@@ -25,6 +26,8 @@ struct TreeNode {
 class Codec {
 public:
   string str;
+  string nullToken = "null";
+  string delimiter = ",";
   // Encodes a tree to a single string.
   string serialize(TreeNode *root) {
     str = traverse(root);
@@ -34,21 +37,35 @@ public:
   /* postorder */
   string traverse(TreeNode *root) {
     if (root == nullptr)
-      return "null";
+      return nullToken;
     string left = traverse(root->left);
     string right = traverse(root->right);
-    return root->val + "," + left + "," + right;
+    return to_string(root->val) + delimiter + left + delimiter + right;
   }
   int idx;
   // Decodes your encoded data to tree.
   TreeNode *deserialize(string data) {
-    vector<string> *list = split(data, ',');
+    vector<string> list;
+    size_t pos = 0;
+    while ((pos = data.find(delimiter)) != string::npos) {
+      list.push_back(data.substr(0, pos));
+      data.erase(0, pos + delimiter.length());
+    }
+    return build(&list);
   }
   TreeNode *build(vector<string> *list) {
-    if (idx > list->size()-1 ) {
+    if (list->empty())
+      return nullptr;
+    if (list->at(0) == nullToken) {
+      list->erase(list->begin());
       return nullptr;
     }
-    
+    TreeNode *root = new TreeNode(stoi(list->at(0)));
+    list->erase(list->begin());
+
+    root->left = build(list);
+    root->right = build(list);
+    return root;
   }
 };
 
@@ -56,3 +73,11 @@ public:
 // Codec ser, deser;
 // TreeNode* ans = deser.deserialize(ser.serialize(root));
 // @lc code=end
+
+int main() {
+  string in = "1,2,null,4,null,null,3,null,null";
+  Codec ser, deser;
+  auto mid = deser.deserialize(in);
+  auto out = ser.serialize(mid);
+  cout << out << endl;
+}
