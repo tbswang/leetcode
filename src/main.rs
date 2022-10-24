d3fc46de8cdebd2abb3eb47fc72be0ea2eede84b
gongtao02 <gongtao02@meituan.com>
2022-10-24 20:37:06 +0800

    wip: rust

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod two_sum;
+
+fn main() {
+    println!("Hello, world!");
+    two_sum::two_sum([1,3], 1)
+}
---
+++ src/two_sum.rs
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=rust
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        return [0, 1].to_vec();
+    }
+}
+// @lc code=end
